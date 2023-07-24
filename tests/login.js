0411328e2ea6f223dce5c715d0b2cd902b872835
GallickGun <gallickgun@GallickGuns-MacBook-Pro.local>
2023-07-24 13:29:31 +0700

    initial commit

---
+++ tests/login.js
@@ -0,0 +1,12 @@
+const { Selector } = require("testcafe");
+
+fixture`Login Suite`
+.page`https://www.saucedemo.com/v1/
+`
+test('Valid Login', async(t)=>{
+    await t
+    .typeText("#user-name", "standard_user")
+    .typeText("#password", "secret_sauce")
+    .click("#login-button")
+    .expect(Selector(".product_label").innerText).eql("Products")
+});
